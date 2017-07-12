10242e6add7344332d280f58a631aac6c410711f
blah <crcooper@outlook.com>
2017-07-12 11:38:27 +0100

    PB/AR: First commit with basic test verifying 5000ms interval on slideshow.

--- tests/fi/iki/jka/JPhotoFrameTest.java
+++ tests/fi/iki/jka/JPhotoFrameTest.java
@@ -1,13 +1,41 @@
 package fi.iki.jka;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.*;
+
+import java.awt.event.ActionEvent;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.junit.Assert.assertThat;
 
-public class JPhotoFrameTest {
+public class JPhotoFrameTest{
+
+    int expectedInterval;
+
     @Test
-    public void placeholder() throws Exception {
-        assertThat(2, equalTo(2));
+    public void verifyIfIntervalIs5000OnSlideShow() throws Exception {
+        ActionEvent event = new ActionEvent("", 0, JPhotoMenu.A_SLIDESHOW);
+
+        JPhotoFrame frame = new JPhotoFrame(){
+            @Override
+            public void runSlideShow(int interval) {
+                expectedInterval = interval;
+//                photos = new JPhotoCollection();
+//                photos.add(1, new JPhoto());
+//                super.runSlideShow(interval);
+            }
+
+            @Override
+            public void setTitle() {
+                //we don't care about the title, so let's leave it empty
+            }
+        };
+        frame.actionPerformed(event);
+
+        assertEquals(5000, expectedInterval);
     }
 }
+
