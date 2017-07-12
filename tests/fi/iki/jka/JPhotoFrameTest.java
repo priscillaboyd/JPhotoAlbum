fdd036886ae26c81a3d21d7bae90e5fd402d9f35
blah <crcooper@outlook.com>
2017-07-12 11:50:54 +0100

    PB/AR: New test to support that we can run slideshow faster

--- tests/fi/iki/jka/JPhotoFrameTest.java
+++ tests/fi/iki/jka/JPhotoFrameTest.java
@@ -16,16 +16,12 @@ public class JPhotoFrameTest{
     int expectedInterval;
 
     @Test
-    public void verifyIfIntervalIs5000OnSlideShow() throws Exception {
+    public void verifyIfInterval5000WorksOnSlideShow() throws Exception {
         ActionEvent event = new ActionEvent("", 0, JPhotoMenu.A_SLIDESHOW);
-
         JPhotoFrame frame = new JPhotoFrame(){
             @Override
             public void runSlideShow(int interval) {
                 expectedInterval = interval;
-//                photos = new JPhotoCollection();
-//                photos.add(1, new JPhoto());
-//                super.runSlideShow(interval);
             }
 
             @Override
@@ -34,8 +30,28 @@ public class JPhotoFrameTest{
             }
         };
         frame.actionPerformed(event);
-
         assertEquals(5000, expectedInterval);
     }
+
+    @Test
+    public void verifyIfFasterIntervalIsUsedForSlideShow() throws Exception{
+        ActionEvent event = new ActionEvent("", 0, JPhotoMenu.A_FAST_SLIDESHOW);
+        JPhotoFrame frame = new JPhotoFrame(){
+
+            @Override
+            public void runSlideShow(int interval) {
+                expectedInterval = interval;
+            }
+
+            @Override
+            public void setTitle() {
+                //we don't care about the title, so let's leave it empty
+            }
+        };
+        frame.actionPerformed(event);
+        assertEquals(2000, expectedInterval);
+    }
+
+
 }
 
